86274865c1fc41903688514a8d7d61408f0a806b
Yasmine Hartung <y.hartung@gmail.com>
2019-03-05 09:29:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime? (num)
+  limit = Math.sqrt(num.abs)
+  divisors = (2..limit).to_a
+  divisors.each do |div|
+    remainder = num % div
+    if remainder == 0
+      return false
+    end
+  end
+  return true
+
+end
